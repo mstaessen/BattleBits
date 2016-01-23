f033a6951b8952099d892b34c442fdcac4b39c0f
Sam Segers <samsegers@hotmail.com>
2016-01-23 20:05:01 +0100

    re add lost file battlebitsgame for possible further use

---
+++ BattleBits.Web/Models/BattleBitsGame.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace BattleBits.Web.Models
+{
+    public class BattleBitsGame
+    {
+        private static readonly Random Random = new Random();
+
+        public long Id { get; set; } 
+
+        public DateTime StartDate { get; set; }
+
+        public TimeSpan Duration { get; set; }
+
+        public byte[] Bytes { get; set; }
+
+        public virtual Competition Competition { get; set; }
+
+        /// <summary>
+        /// EF constructor
+        /// </summary>
+        protected BattleBitsGame() {}
+
+        public BattleBitsGame(int size)
+        {
+            Bytes = new byte[size];
+            Random.NextBytes(Bytes);
+        }
+    }
+}
