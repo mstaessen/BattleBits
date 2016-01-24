875b115f98db5710ba4b44033bfd51521cd571d3
Michiel Staessen <staessenmichiel@gmail.com>
2016-01-24 14:10:02 +0100

    BattleBits game client + Flexbox layout

--- BattleBits.Web/App_Start/BundleConfig.cs
+++ BattleBits.Web/App_Start/BundleConfig.cs
@@ -7,8 +7,10 @@ namespace BattleBits.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap.css"));
+            bundles.Add(new StyleBundle("~/Content/battle-bits").Include("~/Content/battle-bits.css"));
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/lodash").Include("~/Scripts/lodash.min.js"));
             bundles.Add(new ScriptBundle("~/bundles/signalr").Include("~/Scripts/jquery.signalr.min.js"));
             bundles.Add(new ScriptBundle("~/bundles/angular").Include("~/Scripts/angular.min.js", "~/Scripts/angular-route.min.js"));
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.min.js"));
--- BattleBits.Web/Scripts/battle-bits.js
+++ BattleBits.Web/Scripts/battle-bits.js
@@ -3,12 +3,44 @@
     .controller('LeaderboardController', function() {
 
     })
-    .controller('GamePlayController', function() {
+    .controller('GamePlayController', function ($scope, $interval) {
+        $scope.guess = 0;
+        $scope.number = 57;
+        $scope.numbersGuessed = 0;
+        $scope.numbersToGuess = 24;
+        $scope.timeLeft = 45;
 
+        $scope.isBitActive = function (bitPosition) {
+            var mask = (1 << bitPosition);
+            return ($scope.guess & mask) === mask;
+        };
+
+        $scope.toggleBit = function(bitPosition) {
+            $scope.guess ^= (1 << bitPosition);
+        };
+
+        var numberWatcher = $scope.$watch('number', function(newValue) {
+            // Send Guess, if OK, move to next number
+        });
+
+        $interval(function() {
+            $scope.timeLeft--;
+        }, 1000, 45);
     })
-    .controller('GameDisplayController', function() {
+    .controller('GameDisplayController', function($scope) {
 
     })
     .service('GameService', function() {
 
+    })
+    .filter('binary', function() {
+        return function (input, length) {
+            length = length || 8;
+            return _.padStart((input >>> 0).toString(2), length, '0');
+        };
+    })
+    .filter('hex', function() {
+        return function (input) {
+            return "0x" + input.toString(16);
+        };
     });
