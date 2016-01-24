c819d8a22f5a26b685eeb59cd18a158d22fb9fde
Sam Segers <samsegers@hotmail.com>
2016-01-24 16:18:19 +0100

    battle-bits program flow

--- BattleBits.Web/Scripts/battle-bits.js
+++ BattleBits.Web/Scripts/battle-bits.js
@@ -4,26 +4,43 @@
 
     })
     .controller('GamePlayController', function ($scope, $interval) {
+        var timer;
         $scope.guess = 0;
-        $scope.number = 57;
+        // TODO load from server
+        // NOTE: can one of the HEXs be zero? in that case the printing is sometimes wrong
+        $scope.numbers = [21, 59, 192, 204, 153, 99, 66, 12, 199, 200, 222, 250, 21, 59, 192, 204, 153, 99, 66, 12, 199, 200, 222, 250];
         $scope.numbersGuessed = 0;
-        $scope.numbersToGuess = 24;
+        $scope.numbersToGuess = $scope.numbers.length;
         $scope.timeLeft = 45;
+        $scope.number = $scope.numbers[$scope.numbersGuessed];
 
         $scope.isBitActive = function (bitPosition) {
-            var mask = (1 << bitPosition);
-            return ($scope.guess & mask) === mask;
+            return ($scope.guess & (1 << bitPosition)) !== 0;
         };
 
         $scope.toggleBit = function(bitPosition) {
             $scope.guess ^= (1 << bitPosition);
         };
 
-        var numberWatcher = $scope.$watch('number', function(newValue) {
-            // Send Guess, if OK, move to next number
+        $scope.$watch('guess', function (newValue) {
+            if ($scope.timeLeft <= 0) {
+                // TODO no more guesses when time is expired (this may be placed somewhere else?)
+                return;
+            }
+            if (newValue === $scope.number) {
+                // TODO handle correct answer to server
+                if (++$scope.numbersGuessed === $scope.numbersToGuess) {
+                    $interval.cancel(timer);
+                    // TODO Notify that all bits are solved (show results)
+                } else {
+                    // assign next number
+                    $scope.number = $scope.numbers[$scope.numbersGuessed];
+                    $scope.guess = 0;
+                }
+            }
         });
 
-        $interval(function() {
+        timer = $interval(function() {
             $scope.timeLeft--;
         }, 1000, 45);
     })
